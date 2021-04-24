93357b570a007e5430030d84f5384809ec676219
taylor200-tlr <taylor200@freemail.hu>
2021-04-24 23:29:43 +0200

    3. feladat

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -6,12 +6,12 @@ import java.util.Set;
 
 public class DigitsCounter {
     public int getCountOfDigits(String text) {
-        if ((text == null) || (text.equals(""))){
+        if ((text == null) || (text.equals(""))) {
             return 0;
         }
         Set<Character> digits = new HashSet<>();
-        for (char c : text.toCharArray()){
-            if (Character.isDigit(c)){
+        for (char c : text.toCharArray()) {
+            if (Character.isDigit(c)) {
                 digits.add(c);
             }
         }

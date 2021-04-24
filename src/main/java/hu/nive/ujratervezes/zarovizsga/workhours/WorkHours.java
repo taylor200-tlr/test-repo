edafa188854072c1df2f1ab97a1df015462735b8
taylor200-tlr <taylor200@freemail.hu>
2021-04-24 23:16:48 +0200

    3. feladat

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -0,0 +1,33 @@
+package hu.nive.ujratervezes.zarovizsga.workhours;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class WorkHours {
+    private int minWork = 25;
+    private StringBuilder stringBuilder = new StringBuilder();
+
+    public String minWork(String path) {
+        String line;
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(path))) {
+            while ((line = reader.readLine()) != null) {
+                processLine(line);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+        return stringBuilder.toString();
+    }
+
+    private void processLine(String line) {
+        String[] splitted = line.split(",");
+        if (Integer.parseInt(splitted[1]) < minWork) {
+            stringBuilder.delete(0, stringBuilder.length());
+            stringBuilder.append(splitted[0]).append(": ").append(splitted[2]);
+            minWork = Integer.parseInt(splitted[1]);
+        }
+    }
+}
--- src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WorkHoursTest {
 
-//    @Test
-//    void minWork() {
-//        WorkHours workHours = new WorkHours();
-//        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
-//        assertEquals("John Doe: 2021-01-04", result);
-//    }
+    @Test
+    void minWork() {
+        WorkHours workHours = new WorkHours();
+        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
+        assertEquals("John Doe: 2021-01-04", result);
+    }
 }
